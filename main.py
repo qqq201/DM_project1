b2d74ab0bdb76fed90c63273b1d8a74d535eef03
Quy Quach <quyquach201@gmail.com>
2021-11-02 11:52:27 +0700

    Define prototypes

---
+++ main.py
@@ -0,0 +1,62 @@
+import csv
+
+class dataframe:
+    def __init__(self):
+        self.data = []
+        self.attr_id = dict()
+
+    def load_csv(self, file):
+        pass
+
+    def save_csv(self, file):
+        pass
+
+    def attr_missing_value(self):
+        pass
+
+    def nrows_missing_value(self):
+        pass
+
+    def substitute_mean(self):
+        pass
+
+    def substitue_median(self):
+        pass
+
+    def substitue_mode(self):
+        pass
+
+    def remove_attr(self, threshold):
+        pass
+
+    def remove_duplicate(self):
+        pass
+
+    def min_max_normalize(self):
+        pass
+
+    def z_score_normalize(self):
+        pass
+
+    def plus(self, attr1, attr2):
+        pass
+
+    def minus(self, attr1, attr2):
+        pass
+
+    def multiply(self, attr1, attr2):
+        pass
+
+    def divide(self, attr1, attr2):
+        pass
+
+    def calculate(self, expression):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
